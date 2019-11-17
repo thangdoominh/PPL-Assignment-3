f0a0efa6db0a1a98baa014e9587b9c2cf240a92e
Do Minh Thang <dominhthang1999@gmail.com>
2019-11-17 17:51:07 +0700

    Redeclared Function/Parameter

--- upload/src/main/mc/checker/StaticCheck.py
+++ upload/src/main/mc/checker/StaticCheck.py
@@ -15,7 +15,7 @@ class MType:
         self.rettype = rettype
 
 class Symbol:
-    def __init__(self,name,mtype,value = None):
+    def __init__(self,name, mtype, value = None):
         self.name = name
         self.mtype = mtype
         self.value = value
@@ -23,6 +23,7 @@ class Symbol:
 class StaticChecker(BaseVisitor,Utils):
 
     global_envi = [
+
     #Built in Function
         Symbol("getInt", MType([], IntType())),
         Symbol("putInt", MType([IntType()], VoidType())),
@@ -33,6 +34,7 @@ class StaticChecker(BaseVisitor,Utils):
         Symbol("putBool", MType([BoolType()], VoidType())),
         Symbol("putBoolLn", MType([BoolType()], VoidType())),
         Symbol("putString", MType([StringType()], VoidType()))
+
     ]
 
 
@@ -44,6 +46,8 @@ class StaticChecker(BaseVisitor,Utils):
 
     #default return None.
     #If it has exists, return Symbol of the sepecified name in the environment.
+    # name: String
+    # env: List or tuple of Symbol
     def findSymbol(self, name, env):
         result = None
         if type(env) is tuple:
@@ -63,20 +67,93 @@ class StaticChecker(BaseVisitor,Utils):
     def visitProgram(self, ast, c):
         g = c[:]
         for x in ast.decl:
-            self.visit(x, (g, Variable()))
+            if type(x) is VarDecl:
+                self.visit(x, (g, Variable()))
+            else:
+                self.visit(x, (g, Function()))
+
+        mainFunc = self.findSymbol('main', g)
+        if mainFunc is None:
+            raise NoEntryPoint();
 
         #no error (Test print)
         return ['Correct']
 
     # param[0] is the environment
-    # param[1] can be anything, in this case Variable or Parameter
-    def visitVarDecl(self, ast, param):
-        res = Symbol(ast.variable, param[0])
-        check = self.findSymbol(ast.variable, param[0])
+    # param[1] can be anything,
+    # in this case Variable or Parameter
+    def visitVarDecl(self, ast, params):
+        res = Symbol(ast.variable, ast.varType)
+        check = self.findSymbol(ast.variable, params[0])
         if check is None:
-            param[0].append(res)
+            params[0].append(res)
+            return res
         else:
-            raise Redeclared(param[1], res.name)
+            raise Redeclared(params[1], res.name)
+
+    def visitFuncDecl(self, ast, params):
+        localEnvironment = []
+        check = self.findSymbol(ast.name.name, params[0])
+        param = [self.visit(x, (localEnvironment, Parameter())) for x in ast.param]
+        if check is None:
+            res = Symbol(ast.name.name, MType(param, ast.returnType))
+            params[0].append(res)
+
+            return res
+        else:
+            raise Redeclared(Function(), ast.name.name)
+
+    def visitArrayPointerType(self, ast, params):
+        pass
+
+    def visitBinaryOp(self, ast, params):
+        pass
+
+    def visitUnaryOp(self, ast, params):
+        pass
+
+    def visitCallExpr(self, ast, params):
+        pass
+
+    def visitId(self, ast, params):
+        pass
+
+    def visitArrayCell(self, ast, params):
+        pass
+
+    def visitBlock(self, ast, params):
+        pass
+
+    def visitIf(self, ast, params):
+        pass
+
+    def visitFor(self, ast, params):
+        pass
+
+    def visitContinue(self, ast, params):
+        pass
+
+    def visitBreak(self, ast, params):
+        pass
+
+    def visitReturn(self, ast, params):
+        pass
+
+    def visitDowhile(self, ast, params):
+        pass
+
+    def visitIntLiteral(self, ast, params):
+        pass
+
+    def visitFloatLiteral(self, ast, params):
+        pass
+
+    def visitBooleanLiteral(self, ast, params):
+        pass
+
+    def visitStringLiteral(self, ast, params):
+        pass
+
 
 
 
--- upload/src/test/CheckSuite.py
+++ upload/src/test/CheckSuite.py
@@ -13,8 +13,108 @@ class CheckSuite(unittest.TestCase):
 int a;
 int b;
         """
-        expect = ""
+        expect = "No Entry Point"
         self.assertTrue(TestChecker.test(input, expect, 401))
+
+    def test_redeclared_function_10(self):
+        input = """
+int a;
+int b;
+void main(){
+    int a;
+}
+int b()
+{}
+        """
+        expect = "Redeclared Function: b"
+        self.assertTrue(TestChecker.test(input, expect, 410))
+
+    def test_redeclared_function_11(self):
+        input = """
+void main(){
+    int a;
+}
+void a()
+{}
+int a()
+{}
+        """
+        expect = "Redeclared Function: a"
+        self.assertTrue(TestChecker.test(input, expect, 411))
+
+    def test_redeclared_function_12(self):
+        input = """
+int x,y;
+void main(){
+    int a;
+}
+float x()
+{
+    boolean y;
+}
+        """
+        expect = "Redeclared Function: x"
+        self.assertTrue(TestChecker.test(input, expect, 412))
+
+    def test_redeclared_function_12(self):
+        input = """
+void main(){
+    int a;
+}
+float y()
+{
+    boolean x;
+}
+boolean y()
+{}
+        """
+        expect = "Redeclared Function: y"
+        self.assertTrue(TestChecker.test(input, expect, 412))
+
+    def test_redeclared_function_13(self):
+        input = """
+void main(int a, float b){
+    return a;
+}
+float[] a(int b)
+{
+    int c;
+}
+boolean a()
+{
+    int x;
+}
+        """
+        expect = "Redeclared Function: a"
+        self.assertTrue(TestChecker.test(input, expect, 413))
+
+    def test_NoEntryPoint_20(self):
+        input = """
+int a(int a, int b)
+{}
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 420))
+
+    def test_NoEntryPoint_21(self):
+        input = """
+float x;
+int Main(int a, int b)
+{}
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 421))
+
+    def test_NoEntryPoint_22(self):
+        input = """
+float x;
+int MAIN(int a, int b)
+{}
+        """
+        expect = "No Entry Point"
+        self.assertTrue(TestChecker.test(input, expect, 422))
+
+
 '''
     def test_diff_numofparam_stmt(self):
         """More complex program"""
